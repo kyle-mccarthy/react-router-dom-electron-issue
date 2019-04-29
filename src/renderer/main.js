1f6a79c71911baa04fa55ab77d71ac1613e976d3
Kyle McCarthy <kyle@creativeanvil.com>
2019-04-29 11:31:44 -0500

    try with react-router-dom 4

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,25 +3,28 @@ import Home from "./pages/home";
 import First from "./pages/first";
 import Second from "./pages/second";
 import Third from "./pages/third";
-import { HashRouter, Route, NavLink } from "react-router-dom";
+import { HashRouter, Route, NavLink, Switch } from "react-router-dom";
 
 const Main = () => {
   return (
     <div>
       <HashRouter>
-        <div>
-          <NavLink to="/">Home</NavLink> <span>&nbsp;|&nbsp;</span>
-          <NavLink to="/first">First</NavLink> <span>&nbsp;|&nbsp;</span>
-          <NavLink to="/second">Second</NavLink> <span>&nbsp;|&nbsp;</span>
-          <NavLink to="/third">Third</NavLink>
-        </div>
+        <>
+          <div>
+            <NavLink to="/">Home</NavLink> <span>&nbsp;|&nbsp;</span>
+            <NavLink to="/first">First</NavLink> <span>&nbsp;|&nbsp;</span>
+            <NavLink to="/second">Second</NavLink> <span>&nbsp;|&nbsp;</span>
+            <NavLink to="/third">Third</NavLink>
+          </div>
 
-        <hr />
-
-        <Route exact={ true } path="/" component={ Home } />
-        <Route path="/first" component={ First } />
-        <Route path="/second" component={ Second } />
-        <Route path="/third" component={ Third } />
+          <hr />
+          <Switch>
+            <Route exact={ true } path="/" component={ Home } />
+            <Route path="/first" component={ First } />
+            <Route path="/second" component={ Second } />
+            <Route path="/third" component={ Third } />
+          </Switch>
+        </>
       </HashRouter>
     </div>
   );
